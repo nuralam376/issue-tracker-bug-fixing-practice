d35af5af095067dd050c360d3d369063e5ed1876
Nur A Alam <nuraalam376@gmail.com>
2020-08-16 08:32:42 +0600

    changed styles

--- main.js
+++ main.js
@@ -48,11 +48,17 @@ const fetchIssues = () => {
 
 		issuesList.innerHTML += `<div class="well">
                               <h6>Issue ID: ${id} </h6>
-                              <p><span class="label label-info"> ${status} </span></p>
+                              <p><span class="label ${
+																status == "Closed"
+																	? "label-success"
+																	: "label-info"
+															}"> ${status} </span></p>
                               <h3 class = "${status.toLowerCase()}">${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a  href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
+                              <a style = "display : ${
+																status === "Closed" ? "none" : ""
+															}" href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
 	}
